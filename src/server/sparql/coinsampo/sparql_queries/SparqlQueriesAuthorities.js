da78c9679356d00dd7ebb0b4ce655a354135e4f1
HeikkiR <heikki.rantala@aalto.fi>
2024-02-28 11:01:33 +0200

    Add finds to authority and mint instance pages

--- src/server/sparql/coinsampo/sparql_queries/SparqlQueriesAuthorities.js
+++ src/server/sparql/coinsampo/sparql_queries/SparqlQueriesAuthorities.js
@@ -3,45 +3,96 @@ const perspectiveID = 'authorities'
 export const authorityPropertiesInstancePage =
 `   
 {
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND (?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(?id as ?uri__id)
-    BIND(?id as ?uri__dataProviderUrl)
-    BIND(?id as ?uri__prefLabel)
+  ?id skos:prefLabel ?prefLabel__id .
+  BIND (?prefLabel__id as ?prefLabel__prefLabel)
+  FILTER(LANG(?prefLabel__prefLabel) = '<LANG>')
+  BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+  BIND(?id as ?uri__id)
+  BIND(?id as ?uri__dataProviderUrl)
+  BIND(?id as ?uri__prefLabel)
   }
   UNION
   {
   ?id ^coin-schema:authority/coin-schema:material ?material__id .
   ?material__id skos:prefLabel ?material__prefLabel .
+  FILTER(LANG(?material__prefLabel) = '<LANG>')
+  BIND(CONCAT("/materials/page/", REPLACE(STR(?period__id), "^.*\\\\/(.+)", "$1")) AS ?material__dataProviderUrl)
   }
   UNION
   {
-  ?id ^coin-schema:authority/coin-schema:municipality ?municipality__id .
+  ?id ^coin-schema:authority/coin-schema:municipality/:yso ?municipality__id .
   ?municipality__id skos:prefLabel ?municipality__prefLabel .
-  FILTER(LANG(?municipality__prefLabel) = 'fi')
+  BIND(CONCAT("/municipalities/page/", REPLACE(STR(?period__id), "^.*\\\\/(.+)", "$1")) AS ?municipality__dataProviderUrl)
   }
   UNION
   {
   ?id ^coin-schema:authority/coin-schema:denomination ?denomination__id .
   ?denomination__id skos:prefLabel ?denomination__prefLabel .
-  FILTER(LANG(?denomination__prefLabel) = 'fi')
+  FILTER(LANG(?denomination__prefLabel) = '<LANG>')
+  BIND(CONCAT("/denominations/page/", REPLACE(STR(?period__id), "^.*\\\\/(.+)", "$1")) AS ?denomination__dataProviderUrl)
   }
   UNION
   {
   ?id ^coin-schema:authority/coin-schema:period ?period__id .
   ?period__id skos:prefLabel ?period__prefLabel .
+  FILTER(LANG(?period__prefLabel) = '<LANG>')
+  BIND(CONCAT("/periods/page/", REPLACE(STR(?period__id), "^.*\\\\/(.+)", "$1")) AS ?period__dataProviderUrl)
   }
   UNION
   {
-  ?id coin-schema:wikidata/coin-schema:description ?description .
+  ?id coin-schema:description ?descriptionFI .
+  FILTER(LANG(?descriptionFI) = 'fi')
   }
   UNION
   {
-    ?id coin-schema:wikidata/coin-schema:image ?image__id .
-    BIND("Image from wikimedia" AS ?image__description)
-    BIND("Image from wikimedia" AS ?image__title)
-    BIND(?image__id AS ?image__url)
+  ?id coin-schema:description ?descriptionEN .
+  FILTER(LANG(?descriptionEN) = 'en')
   }
+  UNION
+  {
+    ?id coin-schema:wikidata ?wikidata__id .
+    BIND (?wikidata__id AS ?wikidata__prefLabel)
+    BIND (?wikidata__id AS ?wikidata__dataProviderUrl)
+  }
+  UNION
+  {
+    ?id coin-schema:wikipedia ?wikipedia__id .
+    BIND (?wikipedia__id AS ?wikipedia__prefLabel)
+    BIND (?wikipedia__id AS ?wikipedia__dataProviderUrl)
+  }
+  UNION
+  {
+    ?id coin-schema:nomisma ?nomisma__id .
+    BIND (?nomisma__id AS ?nomisma__prefLabel)
+    BIND (?nomisma__id AS ?nomisma__dataProviderUrl)
+  }
+  UNION
+  {
+    ?id coin-schema:numista_id ?numista__id .
+    BIND (?numista__id AS ?numista__prefLabel)
+    BIND (CONCAT('https://en.numista.com/catalogue/ruler.php?id=',?numista__id) AS ?numista__dataProviderUrl)
+  }
+  UNION
+  {
+    ?id coin-schema:biographySampo ?bs__id .
+    BIND (?bs__id AS ?bs__prefLabel)
+    BIND (CONCAT('https://biografiasampo.fi/henkilo/',?bs__id) AS ?bs__dataProviderUrl)
+  }
+  UNION
+  {
+    ?id ^coin-schema:authority ?find__id .
+    ?find__id skos:prefLabel ?find__prefLabel .
+    FILTER(LANG(?find__prefLabel) = '<LANG>')
+    BIND(CONCAT("/finds/page/", REPLACE(STR(?find__id), "^.*\\\\/(.+)", "$1")) AS ?find__dataProviderUrl)
+  }
+  OPTIONAL
+  {
+    ?id coin-schema:image ?imageurlTemp .
+  }
+  BIND(COALESCE(?imageurlTemp,"https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Profile_Silhouette_02.svg/240px-Profile_Silhouette_02.svg.png") as ?image__id)
+  BIND(COALESCE(?imageurlTemp,"https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Profile_Silhouette_02.svg/240px-Profile_Silhouette_02.svg.png") as ?image__url)
+  BIND("Image from Wikimedia commons" as ?image__description)
+  BIND(?image__description AS ?image__title)
 `
 
 export const authorityPropertiesFacetResults = `
@@ -53,26 +104,6 @@ export const authorityPropertiesFacetResults = `
     BIND(?id as ?uri__id)
     BIND(?id as ?uri__dataProviderUrl)
     BIND(?id as ?uri__prefLabel)
-    }
-    UNION
-    {
-    ?id ^coin-schema:authority/coin-schema:material ?material__id .
-    ?material__id skos:prefLabel ?material__prefLabel .
-    FILTER(LANG(?material__prefLabel) = '<LANG>')
-    BIND(CONCAT("/materials/page/", REPLACE(STR(?period__id), "^.*\\\\/(.+)", "$1")) AS ?period__dataProviderUrl)
-    }
-    UNION
-    {
-    ?id ^coin-schema:authority/coin-schema:municipality/:yso ?municipality__id .
-    ?municipality__id skos:prefLabel ?municipality__prefLabel .
-    BIND(CONCAT("/municipalities/page/", REPLACE(STR(?period__id), "^.*\\\\/(.+)", "$1")) AS ?period__dataProviderUrl)
-    }
-    UNION
-    {
-    ?id ^coin-schema:authority/coin-schema:denomination ?denomination__id .
-    ?denomination__id skos:prefLabel ?denomination__prefLabel .
-    FILTER(LANG(?denomination__prefLabel) = '<LANG>')
-    BIND(CONCAT("/denominations/page/", REPLACE(STR(?period__id), "^.*\\\\/(.+)", "$1")) AS ?period__dataProviderUrl)
     }
     UNION
     {
@@ -90,36 +121,6 @@ export const authorityPropertiesFacetResults = `
     {
     ?id coin-schema:description ?descriptionEN .
     FILTER(LANG(?descriptionEN) = 'en')
-    }
-    UNION
-    {
-      ?id coin-schema:wikidata ?wikidata__id .
-      BIND (?wikidata__id AS ?wikidata__prefLabel)
-      BIND (?wikidata__id AS ?wikidata__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id coin-schema:wikipedia ?wikipedia__id .
-      BIND (?wikipedia__id AS ?wikipedia__prefLabel)
-      BIND (?wikipedia__id AS ?wikipedia__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id coin-schema:nomisma ?nomisma__id .
-      BIND (?nomisma__id AS ?nomisma__prefLabel)
-      BIND (?nomisma__id AS ?nomisma__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id coin-schema:numista_id ?numista__id .
-      BIND (?numista__id AS ?numista__prefLabel)
-      BIND (CONCAT('https://en.numista.com/catalogue/ruler.php?id=',?numista__id) AS ?numista__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id coin-schema:biographySampo ?bs__id .
-      BIND (?bs__id AS ?bs__prefLabel)
-      BIND (CONCAT('https://biografiasampo.fi/henkilo/',?bs__id) AS ?bs__dataProviderUrl)
     }
     OPTIONAL
     {
--- src/server/sparql/coinsampo/sparql_queries/SparqlQueriesMints.js
+++ src/server/sparql/coinsampo/sparql_queries/SparqlQueriesMints.js
@@ -1,20 +1,22 @@
 const perspectiveID = 'mints'
 
-export const mintsPropertiesInstancePage =
+export const mintPropertiesInstancePage =
 `   
 {
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND (?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(?id as ?uri__id)
-    BIND(?id as ?uri__dataProviderUrl)
-    BIND(?id as ?uri__prefLabel)
+  ?id skos:prefLabel ?prefLabel__id .
+  BIND (?prefLabel__id as ?prefLabel__prefLabel)
+  FILTER(LANG(?prefLabel__prefLabel) = '<LANG>')
+  BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+  BIND(?id as ?uri__id)
+  BIND(?id as ?uri__dataProviderUrl)
+  BIND(?id as ?uri__prefLabel)
   }
   UNION
   {
   ?id ^coin-schema:mint/coin-schema:material ?material__id .
   ?material__id skos:prefLabel ?material__prefLabel .
   FILTER(LANG(?material__prefLabel) = '<LANG>')
-  BIND(CONCAT("materials/page/", REPLACE(STR(?material__id), "^.*\\\\/(.+)", "$1")) AS ?material__dataProviderUrl)
+  BIND(CONCAT("/materials/page/", REPLACE(STR(?material__id), "^.*\\\\/(.+)", "$1")) AS ?material__dataProviderUrl)
   }
   UNION
   {
@@ -41,10 +43,15 @@ export const mintsPropertiesInstancePage =
   ?id ^coin-schema:mint/coin-schema:period ?period__id .
   ?period__id skos:prefLabel ?period__prefLabel .
   FILTER(LANG(?period__prefLabel) = '<LANG>')
+  BIND(CONCAT("/periods/page/", REPLACE(STR(?authority__id), "^.*\\\\/(.+)", "$1")) AS ?period__dataProviderUrl)
   }
-
-
-
+  UNION
+  {
+    ?id ^coin-schema:mint ?find__id .
+    ?find__id skos:prefLabel ?find__prefLabel .
+    FILTER(LANG(?find__prefLabel) = '<LANG>')
+    BIND(CONCAT("/finds/page/", REPLACE(STR(?find__id), "^.*\\\\/(.+)", "$1")) AS ?find__dataProviderUrl)
+  }
 `
 
 export const mintsPropertiesFacetResults = `
@@ -56,33 +63,6 @@ export const mintsPropertiesFacetResults = `
     BIND(?id as ?uri__id)
     BIND(?id as ?uri__dataProviderUrl)
     BIND(?id as ?uri__prefLabel)
-    }
-    UNION
-    {
-    ?id ^coin-schema:mint/coin-schema:material ?material__id .
-    ?material__id skos:prefLabel ?material__prefLabel .
-    FILTER(LANG(?material__prefLabel) = '<LANG>')
-    BIND(CONCAT("/materials/page/", REPLACE(STR(?material__id), "^.*\\\\/(.+)", "$1")) AS ?material__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id ^coin-schema:mint/coin-schema:municipality/coin-schema:yso ?municipality__id .
-      ?municipality__id skos:prefLabel ?municipality__prefLabel .
-      FILTER(LANG(?municipality__prefLabel) = '<LANG>')
-    }
-    UNION
-    {
-      ?id ^coin-schema:mint/coin-schema:authority ?authority__id .
-      ?authority__id skos:prefLabel ?authority__prefLabel .
-      FILTER(LANG(?authority__prefLabel) = '<LANG>')
-      BIND(CONCAT("/authorities/page/", REPLACE(STR(?authority__id), "^.*\\\\/(.+)", "$1")) AS ?authority__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id ^coin-schema:mint/coin-schema:denomination ?denomination__id .
-      ?denomination__id skos:prefLabel ?denomination__prefLabel .
-      FILTER(LANG(?denomination__prefLabel) = '<LANG>')
-      BIND(CONCAT("/denominations/page/", REPLACE(STR(?authority__id), "^.*\\\\/(.+)", "$1")) AS ?denomination__dataProviderUrl)
     }
     UNION
     {
